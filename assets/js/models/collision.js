1f5a6eb60e4a09a10518bb912ed66abf2d082949
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-01-07 02:59:11 -0500

    Finish colision model

--- assets/js/models/collision.js
+++ assets/js/models/collision.js
@@ -1,7 +1,14 @@
-const Collision = function (x, y, html_id) {
+const Collision = function (x, y, type, col_id) {
+	const html_id = `collision_${col_id}`
 	const initialize = function () {
+		let collide = make_collision('collision_'+type,{
+			style: `top: ${y}px; left: ${x}px`,
+			id: html_id
+		})
 
+		$('#container').append(collide)
 	}
+
 	const make_collision = function (tag, attrs) {
 		let ele = createElement(tag)
 		for(let val in attrs)
